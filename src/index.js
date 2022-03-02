cc6021368c09872544bf3d3552358b864c2d2ad4
andres <wilson1840@hotmail.com>
2022-03-02 16:45:37 -0500

    Changing package.json

--- src/index.js
+++ src/index.js
@@ -6,6 +6,6 @@ app.use(require('./app/routes/ingresos'))
 
 
 app.listen(app.get('port'), () => {
-  console.log('servidor en el puerto: ','http://localhost:3000/login')
+  console.log('servidor en el puerto: ','http://localhost:8080/login')
 
 })
