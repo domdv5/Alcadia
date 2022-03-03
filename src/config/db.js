833df284902ab89d8c2e1d049c925e48be1e660f
andres <wilson1840@hotmail.com>
2022-03-03 15:45:18 -0500

    env heroku

--- src/config/db.js
+++ src/config/db.js
@@ -52,7 +52,7 @@ const pool = mysql.createPool({
   connectionLimit: 100,
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.PASSWORD,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
 })
 
