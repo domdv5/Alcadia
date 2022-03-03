a723ed8b77dd0ccbed5a1a66803ee1d486f144ee
andres <wilson1840@hotmail.com>
2022-03-03 14:10:05 -0500

    dbconfig update reconnect 03/03/2022

--- src/config/db.js
+++ src/config/db.js
@@ -1,5 +1,5 @@
 const mysql = require('mysql');
-const connection = mysql.createConnection({
+/* const connection = mysql.createConnection({
   host: 'us-cdbr-east-05.cleardb.net',
   user: 'bef3b39afd51d3',
   password:'1f2fd213',
@@ -14,4 +14,36 @@ connection.connect((err) => {
   console.log("conectado exitosamente a la BD")
 });
 
-module.exports = connection;
+module.exports = connection; */
+
+var db_config = {
+  host: 'us-cdbr-east-05.cleardb.net',
+    user: 'bef3b39afd51d3',
+    password: '1f2fd213',
+    database: 'heroku_30cba212e18e6ef',
+};
+
+var connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config); 
+                                                  
+
+  connection.connect(function(err) {              
+    if(err) {                                     
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); 
+    }                                     
+  });                                     
+                                           
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { 
+      handleDisconnect();                         
+    } else {                                      
+      throw err;                                  
+    }
+  });
+}
+
+handleDisconnect();
