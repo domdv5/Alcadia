089bf4671d74a99e20aaa7c066d126c5cdae8d3d
andres <wilson1840@hotmail.com>
2022-03-03 16:13:32 -0500

    env heroku

--- src/config/db.js
+++ src/config/db.js
@@ -53,7 +53,7 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  database: process.env.DB_DATABASE
 })
 
 pool.getConnection((err, connection) => {
