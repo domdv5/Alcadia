47a72cf616e2479b1b34e1b59c45d1228ba5f5b8
andres <wilson1840@hotmail.com>
2022-03-16 10:31:59 -0500

    fixing server 16/03/2022

--- src/config/server.js
+++ src/config/server.js
@@ -18,7 +18,7 @@ app.use('/resources', express.static(path.join(__dirname, '../public')));
 
 app.use(session({
   secret:'secret',
-  resave: true,
+  resave: false,
   saveUninitialized: true,
 }));
 
