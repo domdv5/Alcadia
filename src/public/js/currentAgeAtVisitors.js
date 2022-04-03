11347936b412bcb559e41e9519730df33fda4e77
andres <wilson1840@hotmail.com>
2022-04-03 09:02:53 -0500

    changing name files

--- src/public/js/currentAgeAtVisitors.js
+++ src/public/js/currentAgeAtVisitors.js
@@ -1,8 +1,8 @@
 
 
-function ageGenerator() {
+const ageGenerator = () => {
     let date = document.getElementById('fecha').value
-    
+
 
     let yearBirth = parseInt(date.slice(0, 4))
     let monthBirth = parseInt(date.slice(5, 7))
@@ -14,26 +14,19 @@ function ageGenerator() {
     let year = currentTime.getFullYear()
     let moth = currentTime.getMonth() + 1
     let day = currentTime.getDate()
-    let nextAge = year - yearBirth 
+    let nextAge = year - yearBirth
     let currentAge = nextAge - 1
 
-    
-    if (day >= dayBirth  && moth>= monthBirth ) {
+
+    if (day >= dayBirth && moth >= monthBirth) {
         document.getElementById('age').value = nextAge
-         return document.getElementById('age').value
-       
+        return document.getElementById('age').value
+
     } else {
-         document.getElementById('age').value= currentAge
-         return  document.getElementById('age').value
+        document.getElementById('age').value = currentAge
+        return document.getElementById('age').value
     }
 
 }
 
 
- const validarFuncion = ()=>{
-    const edad = ageGenerator()
-
-    console.log(edad);
-}
-
- 
