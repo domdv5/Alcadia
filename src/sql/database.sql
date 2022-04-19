f3b46da4ca66ee9803b211546f07c6089dce079d
andres <wilson1840@hotmail.com>
2022-04-19 12:02:10 -0500

    Adding sql queris

--- src/sql/database.sql
+++ src/sql/database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 14-03-2022 a las 00:39:58
+-- Tiempo de generación: 10-04-2022 a las 23:43:55
 -- Versión del servidor: 10.4.19-MariaDB
 -- Versión de PHP: 7.3.28
 
@@ -50,13 +50,6 @@ CREATE TABLE `actividades` (
   `IdCds` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Volcado de datos para la tabla `actividades`
---
-
-INSERT INTO `actividades` (`id_actividades`, `nombre`, `linea`, `componente`, `secretaria`, `facilitador`, `fecha_inicio`, `fecha_final`, `hora_apertura`, `hora_cierre`, `ciudad`, `comuna_corregimiento`, `barrio_vereda`, `direccion`, `tipo_reunion`, `nombre_proyecto_inversion`, `codigo_proyecto_inversion`, `nombres_plan_desarrollo`, `codigo_indicadores_pdm_pdi`, `IdCds`) VALUES
-(1, 'pintura', 'sdfgsdg', 'dfgdfg', 'dfsdfsdf', 'sdfsdfsd', '2022-12-31', '2022-12-31', '15:49:00', '12:59:00', 'fsdfsf', '16. Belén ', 'sdasd', 'sdfsdfsdf', 'Informativa', 'dfsdf', 34, '1111', 345345, 3);
-
 -- --------------------------------------------------------
 
 --
@@ -66,13 +59,12 @@ INSERT INTO `actividades` (`id_actividades`, `nombre`, `linea`, `componente`, `s
 CREATE TABLE `cds` (
   `IdCds` int(11) NOT NULL,
   `espacio` varchar(255) NOT NULL,
-  `Comuna` varchar(40) NOT NULL,
+  `Comuna` varchar(40) DEFAULT NULL,
   `Nombre` varchar(50) NOT NULL,
-  `Direccion` varchar(50) NOT NULL,
-  `Horario` varchar(100) NOT NULL,
-  `Telefono` int(11) NOT NULL,
+  `Direccion` varchar(50) DEFAULT NULL,
+  `Horario` varchar(100) DEFAULT NULL,
+  `Telefono` varchar(20) NOT NULL,
   `correo` varchar(50) NOT NULL,
-  `NombreCoordinador` varchar(100) NOT NULL,
   `concatenar` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -80,10 +72,9 @@ CREATE TABLE `cds` (
 -- Volcado de datos para la tabla `cds`
 --
 
-INSERT INTO `cds` (`IdCds`, `espacio`, `Comuna`, `Nombre`, `Direccion`, `Horario`, `Telefono`, `correo`, `NombreCoordinador`, `concatenar`) VALUES
-(1, 'ADMINISTRADOR', '60. San Cristobal', 'ZONA SUR', 'prueba', 'prueba', 1111, 'prueba@hotmail.com', 'nury', 'ADMINISTRADOR ZONA SUR'),
-(2, 'ADMINISTRADOR', '60. San Cristobal', 'ZONA NORTE', 'prueba', 'prueba', 1111, 'prueba@hotmail.com', 'prueba', 'ADMINISTRADOR ZONA NORTE'),
-(3, 'CENTRO DE DESARROLO SOCIAL', '16. Belén', 'SAN ANTONIO DE PRADO', 'prueba', 'prueba', 1111, 'prueba@hotmail.com', 'prueba', 'CENTRO DE DESARROLO SOCIAL SAN ANTONIO DE PRADO');
+INSERT INTO `cds` (`IdCds`, `espacio`, `Comuna`, `Nombre`, `Direccion`, `Horario`, `Telefono`, `correo`, `concatenar`) VALUES
+(1, 'ADMINISTRADOR', '60. San Cristobal', 'ZONA SUR', 'prueba', 'prueba', '1111', 'prueba@hotmail.com', 'ADMINISTRADOR ZONA SUR'),
+(16, 'ADMINISTRADOR', NULL, 'ZONA NORTE', '', '', '3003200483', 'prueba@hotmail.com', 'ADMINISTRADOR ZONA NORTE');
 
 -- --------------------------------------------------------
 
@@ -99,13 +90,6 @@ CREATE TABLE `ingreso_visitantes` (
   `IdCds` int(11) NOT NULL,
   `id_actividades` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Volcado de datos para la tabla `ingreso_visitantes`
---
-
-INSERT INTO `ingreso_visitantes` (`id_ingresos`, `cedula`, `fecha`, `hora`, `IdCds`, `id_actividades`) VALUES
-(1, 1128459310, '2022-3-13', '15:52:29', 3, 1);
 
 -- --------------------------------------------------------
 
@@ -127,9 +111,10 @@ CREATE TABLE `logs` (
 CREATE TABLE `usuarios` (
   `id_usuarios` int(11) NOT NULL,
   `cedula` int(11) NOT NULL,
+  `nombre_coordinador` varchar(255) NOT NULL,
   `correo` varchar(100) DEFAULT NULL,
   `pass` varchar(255) DEFAULT NULL,
-  `telefono` int(11) NOT NULL,
+  `telefono` varchar(20) NOT NULL,
   `rol` tinyint(1) DEFAULT NULL,
   `IdCds` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -138,10 +123,9 @@ CREATE TABLE `usuarios` (
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`id_usuarios`, `cedula`, `correo`, `pass`, `telefono`, `rol`, `IdCds`) VALUES
-(4, 1234567, 'prueba@hotmail.com', '$2a$08$1wIneBmaB8unk0KS00oX2e7oilzYYMkHQJyvBJurX7yAZ3pfopAbm', 1111, 1, 1),
-(5, 7654321, 'prueba@hotmail.com', '$2a$08$IstFs4g1VGVbP58gD7dQIOO.uiBJ85bwJmUjOyWBHpgQTFG6psn4W', 1231231231, 0, 3),
-(6, 1111111, 'prueba@hotmail.com', '$2a$08$/riGbQGL/eHeI01K6S3LAeWrFwqn7kzPBZuW4WkVX4BIpSXoEJNwi', 0, 0, 3);
+INSERT INTO `usuarios` (`id_usuarios`, `cedula`, `nombre_coordinador`, `correo`, `pass`, `telefono`, `rol`, `IdCds`) VALUES
+(1, 1234567, 'Andres Calderon', 'prueba@hotmail.com', '$2a$08$1wIneBmaB8unk0KS00oX2e7oilzYYMkHQJyvBJurX7yAZ3pfopAbm', '3003200483', 1, 1),
+(2, 1128459310, 'Andrés Calderón', 'wilson1840@hotmail.com', '$2a$08$5j6UCFEoV27.uO/jVoMkMOc.mEcDD1kV79E3tsBXV3KPUWc4SeKvO', '3003200483', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -163,17 +147,10 @@ CREATE TABLE `visitantes` (
   `degradacion_territorial` varchar(255) NOT NULL,
   `correo_electronico` varchar(255) NOT NULL,
   `organizacion` varchar(255) NOT NULL,
-  `telefono_celular` int(45) NOT NULL,
+  `telefono_celular` varchar(45) NOT NULL,
   `sexo` char(45) NOT NULL,
   `IdCds` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Volcado de datos para la tabla `visitantes`
---
-
-INSERT INTO `visitantes` (`id_visita`, `tipo_documento`, `numero_documento`, `nombre_completo`, `secretaria_entidad`, `ocupacion`, `fecha_nacimiento`, `edad`, `enfoque_diferencial`, `derechos`, `degradacion_territorial`, `correo_electronico`, `organizacion`, `telefono_celular`, `sexo`, `IdCds`) VALUES
-(1, 'cedula de ciudadania', 1128459310, 'jonathan zapat', 'ds', 'adsds', '2022-12-31', -1, 'ROM', 'Población victima', '1. Popular', 'wilson1840@hotmail.com', 'sdfsdf', 3760193, 'Masculino', 3);
 
 --
 -- Índices para tablas volcadas
@@ -228,31 +205,31 @@ ALTER TABLE `visitantes`
 -- AUTO_INCREMENT de la tabla `actividades`
 --
 ALTER TABLE `actividades`
-  MODIFY `id_actividades` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_actividades` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `cds`
 --
 ALTER TABLE `cds`
-  MODIFY `IdCds` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `IdCds` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT de la tabla `ingreso_visitantes`
 --
 ALTER TABLE `ingreso_visitantes`
-  MODIFY `id_ingresos` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_ingresos` int(255) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id_usuarios` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_usuarios` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `visitantes`
 --
 ALTER TABLE `visitantes`
-  MODIFY `id_visita` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_visita` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Restricciones para tablas volcadas
