36d6fa631a958fe9d1a6599cb857db5a1180b128
andres <wilson1840@hotmail.com>
2022-04-19 11:37:55 -0500

    adding automatic rol

--- src/public/js/putValueToRolAtUsers.js
+++ src/public/js/putValueToRolAtUsers.js
@@ -1,7 +1,7 @@
 const getSelectValue = () => {
-    const value = document.getElementById("tipo").options[document.getElementById('tipo').selectedIndex].text
+    const value = document.getElementById("tipo").options[document.getElementById('tipo').selectedIndex].innerHTML
 
-
+    
     if (value.startsWith('ADMINISTRADOR')) {
         document.getElementById("item1").setAttribute('value', '1')
         document.getElementById("item1").innerHTML = 'Administrador'
